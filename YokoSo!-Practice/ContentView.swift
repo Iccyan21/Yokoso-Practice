41b1d49eeeca35b11ec0ded5490dc705dbcb9819
Iccyan21 <iccyan0429@gmail.com>
2024-02-29 20:36:38 +0900

    first commit

---
+++ YokoSo!-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YokoSo!-Practice
+//
+//  Created by 水原　樹 on 2024/02/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YokoSo!-Practice/YokoSo__PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YokoSo__PracticeApp.swift
+//  YokoSo!-Practice
+//
+//  Created by 水原　樹 on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct YokoSo__PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
